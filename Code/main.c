cbede2c9f451f8e3db16d22de8b5a3fbb5a62e3e
UshioA <shiosshio@outlook.com>
2023-04-24 14:05:54 +0800

    [backup]
    Signed-off-by: UshioA <shiosshio@outlook.com>

--- Code/main.c
+++ Code/main.c
@@ -23,16 +23,16 @@ int main(int argc, char **argv) {
   if (!yyparse(ast_root)) {
     if (!parerr) {
       do_semantic(ast_root);
-    }
-    if (!semanerr) {
-      init_ircode();
-      translate_program(ast_root);
-      if (argc <= 2)
-        ff = stdout;
-      else
-        ff = fopen(argv[2], "w");
-      init_file(ff);
-      dump_code();
+      if (!semanerr) {
+        init_ircode();
+        translate_program(ast_root);
+        if (argc <= 2)
+          ff = stdout;
+        else
+          ff = fopen(argv[2], "w");
+        init_file(ff);
+        dump_code();
+      }
     }
   }
   return 0;
