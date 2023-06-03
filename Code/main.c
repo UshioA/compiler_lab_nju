0f105db1f45f9debdffc119eeb9a86c136a42dcf
UshioA <shiosshio@outlook.com>
2023-06-03 22:18:28 +0800

    [naive]: plain read and print
    Signed-off-by: UshioA <shiosshio@outlook.com>

--- Code/main.c
+++ Code/main.c
@@ -11,6 +11,7 @@ void yyrestart(FILE *);
 int yyparse(ast_node *);
 extern int parerr;
 extern int semanerr;
+static char buf[255];
 int main(int argc, char **argv) {
   FILE *f;
   FILE *ff;
@@ -22,26 +23,29 @@ int main(int argc, char **argv) {
     perror(argv[1]);
     return -1;
   }
-  yyrestart(f);
-  if (!yyparse(ast_root)) {
-    if (!parerr) {
-      do_semantic(ast_root);
-      if (!semanerr) {
-        init_ircode();
-        translate_program(ast_root);
-        if (argc <= 2)
-          ff = stdout;
-        else
-          ff = fopen(argv[2], "w");
-        // init_file(stdout);
-        // dump_code();
-        init_codefile(ff);
-        gencode();
-        // for (int i = 0; i < cfg_list->length; ++i) {
-        //   cfg_dump(stdout, arr_get(i, cfg_list));
-        // }
-      }
-    }
+  if (argc <= 2)
+    ff = stdout;
+  else
+    ff = fopen(argv[2], "w");
+  while (fscanf(f, "%[^\n] ", buf) != EOF) {
+    fprintf(ff, "%s\n", buf);
   }
+  // yyrestart(f);
+  // if (!yyparse(ast_root)) {
+  //   if (!parerr) {
+  //     do_semantic(ast_root);
+  //     if (!semanerr) {
+  //       init_ircode();
+  //       translate_program(ast_root);
+  //       // init_file(stdout);
+  //       // dump_code();
+  //       init_codefile(ff);
+  //       gencode();
+  //       // for (int i = 0; i < cfg_list->length; ++i) {
+  //       //   cfg_dump(stdout, arr_get(i, cfg_list));
+  //       // }
+  //     }
+  //   }
+  // }
   return 0;
 }
